dd85de34b9406ce98f3a6ebe71661ad865a4f4fd
Muhammad Asad <asadrajputq518@gmail.com>
2023-08-23 16:02:51 +0500

    final

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: file_names
 
 class Product {
   final String title, image, id, details;
@@ -10,6 +10,7 @@ class Product {
       required this.details});
 }
 
+// ignore: non_constant_identifier_names
 List<Product> demo_products = [
   Product(
     id: '1',
